97e4b3c7eb164a33367e3790d1177fd7846575e0
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-12-20 19:54:46 +0530

    update

--- patterns_que/diamond.py
+++ patterns_que/diamond.py
@@ -1,23 +1,7 @@
 row = 5
 
+for i in range(1, row + 1):
+    print(" "*(row - i),"*"*((2*i) - 1))
+
 for i in range(1,row + 1):
-
-    for space in range(row -  i):
-        print(" ",end="")
-    
-    for star in range(2 * i - 1):
-        print("*",end="")
-    
-    print()
-
-rows = 5
-
-for i in range(1,rows + 1):
-
-    for space in range(i-1):
-        print(" ",end="")
-
-    for star in range(2*(rows - i) + 1):
-        print("*",end="")
-
-    print()
+    print(" "*(i - 1), "*"*(2*(row - i) + 1))
